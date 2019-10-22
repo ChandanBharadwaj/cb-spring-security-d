a4a5db6c40a50aa6dc7b9678490b45be75bf48c6
Chandan Bharadwaj <kolluruchandanbharadwaj@gmail.com>
2019-10-22 11:00:10 +0530

    Add files via upload

---
+++ src/main/java/com/sai/CbSpringSecurtiyDApplication.java
@@ -0,0 +1,13 @@
+package com.sai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbSpringSecurtiyDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbSpringSecurtiyDApplication.class, args);
+	}
+
+}
